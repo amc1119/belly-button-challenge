8c4b662c7a1bb7d36e0666195a715843db780745
amc1119 <alexis@Alexiss-MBP.lan>
2024-02-06 01:22:41 -0500

    Added test subject info to drop down menu. Created multiple functions to display and plot data.Some plot formatting done.

--- static/js/app.js
+++ static/js/app.js
@@ -1,61 +1,113 @@
 // Get the url endpoint
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Retrieve JSON data
-d3.json(url). then(function(data) {
-    console.log(data);
-});
-
-let sample_values = 
-let otu_ids =
-let otu_labels = 
-
-// Initializes the page with a default plot
 function init() {
-    let data = [{
-      values: sample_values,
-      labels: otu_ids,
-      type: "bar",
-      orientation: 'h',
-      text: hover_text,
-      hoverinfo: otu_labels
-    }];
-  
-    let layout = {
-      height: 600,
-      width: 800
-    };
-  
-    Plotly.newPlot("bar", data, layout);
+  let selector = d3.select("#selDataset")
+  // Retrieve JSON data
+  d3.json(url). then(function(bellyButton) {
+  let samples = bellyButton.names;
+  for (let i =0; i < samples.length; i++) {
+    selector.append("option").text(samples[i]).property("value", samples[i]);
   }
-  
-  // Call updatePlotly() when a change takes place 
-  d3.selectAll("#selDataset").on("change", newsamplePlot);
-  
-  // This function is called when a dropdown menu item is selected
-  function newsamplePlot() {
-    // Use D3 to select the dropdown menu
-    let dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    let otuData = dropdownMenu.property("sample_values");
-  
-    // Initialize x and y arrays
-    let x = [];
-    let y = [];
-  
-    if (otuData === 'dataset1') {
-      x = [1, 2, 3, 4, 5];
-      y = [1, 2, 4, 8, 16];
+  console.log(bellyButton);
+
+  let firstSubject = samples[0];
+
+  info(firstSubject);
+  charts(firstSubject);
+  })
+}
+
+init();
+
+function optionChanged(subjectID) {
+  info(subjectID);
+  charts(subjectID);
+}
+
+function info(sampleItem) {
+  d3.json(url). then(function(bellyButton) {
+    let meta = bellyButton.metadata;
+    let metaArray = meta.filter(obj => obj.id == sampleItem);
+    let metaResults = metaArray[0];
+
+    console.log(metaResults);
+
+    let table = d3.select("#sample-metadata");
+    table.html("");
+
+    for(key in metaResults) {
+      table.append("h6").text(`${key}: ${metaResults[key]}`)
     }
-  
-    else if (otuData === 'dataset2') {
-      x = [10, 20, 30, 40, 50];
-      y = [1, 10, 100, 1000, 10000];
+  })
+}
+
+function charts(chartData) {
+  d3.json(url). then(function(bellyButton) {
+    let meta = bellyButton.metadata;
+    let metaArray = meta.filter(obj => obj.id == chartData);
+    let metaResults = metaArray[0];
+    let washFrequency = metaResults.wfreq;
+
+    let sample = bellyButton.samples;
+    let sampleArray = sample.filter(obj => obj.id == chartData);
+    let sampleResults = sampleArray[0];
+
+    let otu_ids = sampleResults.otu_ids;
+    let otu_labels = sampleResults.otu_labels;
+    let sample_values = sampleResults.sample_values;
+
+    // Trace for the OTU Data 
+    let trace1 = {
+    x: sample_values.slice(0, 10).reverse(),
+    y: otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`).reverse(),
+    text: otu_labels.slice(0, 10).reverse(),
+    type: "bar",
+    orientation: 'h'
+  };
+
+  // Data trace array
+  let data = [trace1];
+
+  let layout = {
+    margin: {
+      l: 130,
+      r: 30,
+      t: 30,
+      b: 100
     }
+  };
+
+  Plotly.newPlot("bar", data, layout);
+  var bubbleTrace = {
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    mode: 'markers',
+    marker: {
+      color: otu_ids,
+      colorscale: "Earth",
+      size: sample_values
+    }
+  };
   
-    // Note the extra brackets around 'x' and 'y'
-    Plotly.restyle("plot", "x", [x]);
-    Plotly.restyle("plot", "y", [y]);
-  }
+  var bubbleData = [bubbleTrace];
   
-  init();
+  var bubbleLayout = {
+    xlabel: 'OTU ID',
+    showlegend: false,
+    margin: {
+      l: 50,
+      r: 10,
+      t: 100,
+      b: 100
+    }
+  };
+  
+  Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+  
+  })
+
+}
+
+  
